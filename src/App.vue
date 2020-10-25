0a4304c59f0e3e8ae90eb3d00c851a2fa6e79660
qiushunmeng <qiu3291002845@gmail.com>
2020-10-25 11:38:32 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <keep-alive>
+        <component :is="Component"></component>
+      </keep-alive>
+    </transition>
+  </router-view>
+</template>
+
+<style lang="scss">
+@import "./assets/css/fade.scss";
+@import "./assets/css/global.scss";
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Button>你好</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Button } from "ant-design-vue";
+export default defineComponent({
+  name: "Home",
+  components: {
+    Button,
+  },
+});
+</script>
